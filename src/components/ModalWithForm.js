2985914aec57d6bbf941897ae0baa607c30a6e57
Lil Vanilla <kalebrwalter@gmail.com>
2024-03-23 06:15:40 -0700

    bugfix/ delete unnecessary code

--- src/components/ModalWithForm.js
+++ src/components/ModalWithForm.js
@@ -34,7 +34,6 @@ export default class ModalWithForm extends Modal {
     this._form.addEventListener("submit", (e) => {
       e.preventDefault();
       this._handleFormSubmit(this._getInputValues());
-      this.close();
     });
   }
 
